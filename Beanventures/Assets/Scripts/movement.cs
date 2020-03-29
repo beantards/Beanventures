f0580f42bde9c30ead1b6d230125f5e7678410d2
jolathesurvivor <jolathesurvivor@gmail.com>
2020-03-29 16:01:16 +0300

    Movement start

---
+++ Beanventures/Assets/Scripts/movement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void update()
+    {
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+    }
+}
