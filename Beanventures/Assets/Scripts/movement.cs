1414b12792c38be52d4838976613b1c8388e8cd5
Griffihr <45300435+Griffihr@users.noreply.github.com>
2020-03-29 20:26:09 +0300

    Character controller

--- Beanventures/Assets/Scripts/movement.cs
+++ Beanventures/Assets/Scripts/movement.cs
@@ -3,35 +3,29 @@ using System.Collections;
 
 public class movement : MonoBehaviour
 {
-    CharacterController characterController;
+    public CharacterController controller;
 
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-
     private Vector3 moveDirection = Vector3.zero;
+    public float gravity = 20f;
 
-    void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-    }
 
     void Update()
     {
-        if (characterController.isGrounded)
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * y;
+        controller.Move(move * speed * Time.deltaTime);
+
+        /*if (controller.isGrounded && Input.GetButtonDown("Space"))
         {
-
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
-
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
+            moveDirection.y = jumpSpeed;
         }
-
         moveDirection.y -= gravity * Time.deltaTime;
-
-        characterController.Move(moveDirection * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+        */
     }
 }
